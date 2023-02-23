7e5b74fd5b4516bc7b60b2772061b613a0b1aeea
Guilherme Rio Belo <guilhermerioa32@gmail.com>
2023-02-23 00:37:09 -0300

    método delete e update parcial

--- src/main/java/br/unitins/resource/CafeteriaResource.java
+++ src/main/java/br/unitins/resource/CafeteriaResource.java
@@ -5,13 +5,18 @@ import java.util.List;
 
 import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
+import javax.ws.rs.PATCH;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+
 @Path("/cafeteria")
 public class CafeteriaResource {
+    // Insere informações no Banco
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Transactional
@@ -20,11 +25,45 @@ public class CafeteriaResource {
         cafeteria.persist();
         return cafeteria;
     }
+    
+    // Busca todos os Objetos dentro de uma Lista
     @GET
     @Path("/todas")
     @Produces(MediaType.APPLICATION_JSON)
     public List<Cafeteria> getCoffee() {
         return Cafeteria.findAll().list();
-    } 
-}
+    }
 
+    // Atualizando parcialmente o Objeto
+    @PATCH
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Transactional
+    public Cafeteria updateParcial(@PathParam("id")Long id, Cafeteria cafeteria) {
+        Cafeteria attCafeteria = Cafeteria.findById(id);
+        attCafeteria.setTipoBebida(cafeteria.getTipoBebida());
+        attCafeteria.setTipoSalgado(cafeteria.getTipoSalgado());
+        return attCafeteria;
+    }
+
+    // Busca o id específico
+    @GET
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Cafeteria cafeteriaId(@PathParam("id")Long id) {
+        return Cafeteria.findById(id);
+     }
+
+     // Deleta Objeto do id informado
+    @DELETE
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Transactional
+     public Cafeteria cafeteriaDel(@PathParam("id")Long id, Cafeteria cafeteria) {
+        Cafeteria del = Cafeteria.findById(id);
+        del.delete();
+        return del;
+     }
+}
